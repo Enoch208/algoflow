efbd234d71e2e0924a89e1383e6e72a210ca20ec
Enoch208 <ifeoluwaidowu2008@gmail.com>
2025-06-26 01:44:07 -0700

    added analytics tags in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics/>
       <body className={inter.className}>
         <Suspense fallback={null}>
           <ProgressBar />
